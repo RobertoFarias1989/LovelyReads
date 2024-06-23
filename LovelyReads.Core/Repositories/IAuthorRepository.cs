5913820733359d75c5dafbd90c9f413c01a5d287
RobertoFarias1989 <robertosf1989@gmail.com>
2024-06-23 09:05:13 -0300

    feature: added entities, value objects, repositories, unitofwork

--- LovelyReads.Core/Repositories/IAuthorRepository.cs
+++ LovelyReads.Core/Repositories/IAuthorRepository.cs
@@ -2,13 +2,14 @@
 
 namespace LovelyReads.Core.Repositories;
 
-public interface AuthorRepository
+public interface IAuthorRepository
 {
     //o que eu vou poder fazer com a minha entidade?
     //adicionar, atualizar, listar todos, listar um, deletar quem sabe...
-
-    Task AddAsync(Author author);
-    Task UpdateAsync(Author author);
     Task<List<Author>> GetAllAsync();
     Task<Author> GetByIdAsync(int id);
+    Task<Author> GetDetailsByIdAsync(int id);
+    Task AddAsync(Author author);
+    Task UpdateAsync(Author author);
+
 }
