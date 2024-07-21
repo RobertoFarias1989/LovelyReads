3457054211df1769db74d793d423b410ce9afe09
RobertoFarias1989 <robertosf1989@gmail.com>
2024-07-20 22:48:16 -0300

    wip: implementing api layer;

---
+++ LovelyReads.Application/ExthensionMethods/ApplicationDependencyInjection.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using LovelyReads.Application.Author.Commands.CreateAuthor;
+using LovelyReads.Application.Author.Validators;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace LovelyReads.Application.ExthensionMethods
+{
+    public static class ApplicationDependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediator()
+                .AddValidator();
+
+            return services;
+        }
+
+        public static IServiceCollection AddMediator(this IServiceCollection services) 
+        {
+            services.AddMediatR(typeof(CreateAuthorCommand));
+
+            return services; 
+            
+        }
+
+        public static IServiceCollection AddValidator(this IServiceCollection services) 
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            //services.AddFluentValidationAutoValidation();
+            services.AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateAuthorCommandValidator>());
+
+            return services;
+
+        }
+    }
+}
---
+++ LovelyReads.Infrastructure/ExthensionMethods/InfraDependencyInjection.cs
@@ -0,0 +1,50 @@
+﻿using LovelyReads.Core.Repositories;
+using LovelyReads.Infrastructure.Persistence;
+using LovelyReads.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LovelyReads.Infrastructure.ExthensionMethods
+{
+    public static class InfraDependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddRepositories()
+                .AddUnitOfWork();
+
+            return services;
+            
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services) 
+        {
+            services.AddScoped<IAuthorRepository, AuthorRepository>();
+            services.AddScoped<IBookRepository, BookRepository>();
+            services.AddScoped<IBookReviewRepository, BookReviewRepository>();
+            services.AddScoped<IGenreRepository, GenreRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration) 
+        {
+            var connectionString = configuration.GetConnectionString("LovelyReads");
+
+            services.AddDbContext<LovelyReadsDbContext>(options => options.UseSqlServer(connectionString));
+
+            return services;
+        
+        }
+
+        public static IServiceCollection AddUnitOfWork(this IServiceCollection services) 
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        
+        }
+    }
+}
